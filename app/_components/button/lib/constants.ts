9be7c06a34d8118a3a3803a5577cd258522389dd
Marcus Ng <marcusngwj@msn.com>
2026-04-18 00:01:56 +0800

    add button constants

---
+++ app/_components/button/lib/constants.ts
@@ -0,0 +1,18 @@
+export const buttonVariant = {
+  Default: "default",
+  Secondary: "secondary",
+  Outline: "outline",
+  Destructive: "destructive",
+  Ghost: "ghost",
+  Link: "link"
+} as const;
+
+export const buttonSize = {
+  Default: "default",
+  Small: "sm",
+  Large: "lg",
+  Icon: "icon",
+  IconExtraSmall: "icon-xs",
+  IconSmall: "icon-sm",
+  IconLarge: "icon-lg",
+} as const;
