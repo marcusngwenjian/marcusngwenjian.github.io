150e9d7a673ff320d71329a763723e59310f4857
Marcus Ng <marcusngwj@msn.com>
2026-04-22 14:59:35 +0800

    enhance button component

--- app/_components/button/index.ts
+++ app/_components/button/index.ts
@@ -1,2 +1,4 @@
 export * from "./Button";
 export * from "./lib/constants";
+export * from "./lib/variants";
+export * from "./lib/types";
---
+++ app/_components/button/lib/types.ts
@@ -0,0 +1,5 @@
+import { buttonSize, buttonVariant } from "./constants";
+
+export type ButtonVariant = (typeof buttonVariant)[keyof typeof buttonVariant];
+
+export type ButtonSize = (typeof buttonSize)[keyof typeof buttonSize];
